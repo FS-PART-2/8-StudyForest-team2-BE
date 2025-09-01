9edb4e76438f84dd60dc258ba211beea8726274b
Dongyoung.Kim <cuckooolly@gmail.com>
2025-09-01 10:28:34 +0900

    fix: AI 코드리뷰 수정사항 반영
    - 비밀번호 누락에 대한, 에러 핸들링 코드 추가
    - 비밀번호를 Body에 저장하도록, 코드 및 http 파일 변경

--- src/api/structs.js
+++ src/api/structs.js
@@ -4,7 +4,7 @@ export const createStudy = s.object({
   nick: s.size(s.string(), 1, 30),
   name: s.size(s.string(), 1, 100),
   content: s.size(s.string(), 0, 2000), // 본문 상한
-  img: s.pattern(s.string(), /^\/img\/[a-z0-9\-_.]+\.png$/i), // 허용 경로/확장자
+  img: s.string(), // 허용 경로/확장자
   password: s.size(s.string(), 1, 64),
   checkPassword: s.size(s.string(), 1, 64),
   isActive: s.defaulted(s.boolean(), true), // 기본값 부여
