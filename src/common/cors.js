982911e07da7f168dd1c1fcb35826d1ddc36e84d
seongEun95 <dsa02070@naver.com>
2025-09-05 11:43:06 +0900

    fix: CORS 설정 도메인 수정

--- src/common/cors.js
+++ src/common/cors.js
@@ -9,9 +9,9 @@ const corsOptions = {
     'https://eight-studyforest-team2-be.onrender.com',
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://mindmeld-fe.vercel.app/',
-    'https://8-study-forest-team2-fe-git-develop-seongeun95s-projects.vercel.app/',
-    'https://8-study-forest-team2-ccu0vb250-seongeun95s-projects.vercel.app/',
+    'https://mindmeld-fe.vercel.app',
+    'https://8-study-forest-team2-fe-git-develop-seongeun95s-projects.vercel.app',
+    'https://8-study-forest-team2-ccu0vb250-seongeun95s-projects.vercel.app',
   ], // 허용할 도메인
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // 허용할 HTTP 메서드
   allowedHeaders: ['Content-Type', 'Authorization', 'x-study-password'], // 허용할 헤더
